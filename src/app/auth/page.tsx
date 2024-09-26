0680b3e1086c5bab7ac25efa635eb3fe404e773c
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-09-26 09:54:31 +0700

    feat: added auth page

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,18 @@
+
+
+
+function Page() {
+
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+        </div>
+    );
+}
+
+
+export default Page;
