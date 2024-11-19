96c25ac82d8d4e6048b6cc0ea991255dbab23c71
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-11-19 13:10:32 +0700

    feat: changed logout redirect logic

--- src/app/logout/_components/logout-content.tsx
+++ src/app/logout/_components/logout-content.tsx
@@ -1,13 +1,20 @@
 "use client";
 import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import { FC, useLayoutEffect } from "react";
 
 const LogoutContent: FC = () => {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await signOut({
+      redirect: false,
+    });
+
+    router.push("/login");
+  };
   useLayoutEffect(() => {
-    signOut({
-      redirect: true,
-      callbackUrl: "/login",
-    }).then();
+    handleSignOut();
   }, []);
   return <></>;
 };
